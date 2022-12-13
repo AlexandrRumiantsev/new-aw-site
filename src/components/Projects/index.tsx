95b8864f60c6c715e691bf92faab6599d67aea84
alex <tte577es7@yandex.ru>
2022-12-13 21:52:03 +0300

    запросил данные из апишки, закинул в стор и вывел в компоненте

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -9,20 +9,22 @@ import projectStore from '../../stores/project-store/project-store';
 
 import SwipeableViews from 'react-swipeable-views';
 
-export const Projects = observer(() => {
-
+export const Projects = observer((data) => {
+  projectStore.list
+  console.log('list', projectStore.list)
   return <ProjectsWrapper>
     <h1>Проекты</h1>
+    {
+                  projectStore.list.map(el => 
+                    <h1>{el.title}</h1>
+                  )
+                }
     <ImgContainer>
       <DisplayImg src={Imgs}>
         <DisplayWrapper>
             <SliderCutter >
               <SwipeableViews enableMouseEvents>
-                {
-                  projectStore.list.map(el => 
-                    el.title
-                  )
-                }
+                
               </SwipeableViews>
             </SliderCutter>
         </DisplayWrapper>
--- src/stores/project-store/project-store.tsx
+++ src/stores/project-store/project-store.tsx
@@ -1,18 +1,35 @@
-import { observable} from 'mobx';
+import { observable, computed, action } from 'mobx';
 
 class ProjectStore {
-  @observable list;
+  @observable list = [];
 
   constructor() {
-    this.list = [
-      {
-        'title': 1
-      },
-      {
-        'title': 2
+    this.load()
+  }
+
+  @action setValues(values) {
+    this.list.push(values)
+  }
+
+  @action('hide left panel')
+  async load() {
+    
+    let response = await fetch('http://api.alexweber.ru/wp-json/wp/v2/get_portfolio_all', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8'
       }
-    ];
+    });
+    
+    const arrayProject = await response.json()
+    arrayProject.map(project => {
+        this.setValues(project)
+    })
+    
   }
 }
 
-export default new ProjectStore();
+const projectStore = new ProjectStore();
+
+export default projectStore;
+export { ProjectStore };
